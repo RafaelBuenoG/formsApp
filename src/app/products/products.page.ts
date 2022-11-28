bd5992600fd302b4da7f6ddfae4950d77c83c600
Rafael Bueno <rafaelbuenog2020@gmail.com>
2022-11-28 18:29:56 -0300

    Products Page

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
@@ -7,12 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPage implements OnInit {
 
+  productsForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    description: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+    date: ['', Validators.required],
+    price: ['', Validators.required]
+  });
+
   mensagensErro = {
-    name: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'minlenght', aviso: 'O campo deve ter no mínimo 3 dígitos'}],
-    description: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}, {tipo: 'minlength', aviso: 'O campo deve ter no mínimo 20 dígitos'}],
+    name: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}],
+    description: [{tipo: 'required', aviso: 'O campo não pode estar vazio'},
+    {tipo: 'minlength', aviso: 'O campo deve ter no mínimo 10 dígitos'}],
+    date: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}],
+    price: [{tipo: 'required', aviso: 'O campo não pode estar vazio'}]
   };
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  get name(){
+    return this.productsForm.get('name');
+  }
+
+  get description(){
+    return this.productsForm.get('description');
+  }
+
+  get date(){
+    return this.productsForm.get('date');
+  }
+
+  get price(){
+    return this.productsForm.get('price');
+  }
 
   ngOnInit() {
   }
